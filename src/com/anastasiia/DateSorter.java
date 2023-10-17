b02693a3c7302bc6ac28fbd5fffdefb974f62954
AnastasiiaTkachuk <anastaisha.580@gmail.com>
2023-10-17 11:37:03 +0200

    Initial commit

---
+++ src/com/anastasiia/DateSorter.java
@@ -0,0 +1,64 @@
+package com.anastasiia;
+
+import java.time.LocalDate;
+import java.util.*;
+
+/**
+ * Marking will be based upon producing a readable, well engineered solution rather than factors
+ * such as speed of processing or other performance-based optimizations, which are less
+ * important.
+ *
+ *
+ package sample;
+
+ import java.time.LocalDate;
+ import java.util.Collection;
+ import java.util.List;
+
+ /**
+ * Marking will be based upon producing a readable, well engineered solution rather than factors
+ * such as speed of processing or other performance-based optimizations, which are less
+ * important.
+ *
+ * Implement in single class. Don't chane constructor and signature method.
+ */
+public class DateSorter {
+
+    /**
+     * The implementation of this method should sort dates.
+     * The output should be in the following order:
+     * Dates with an 'r' in the month,
+     * sorted ascending (first to last),
+     * then dates without an 'r' in the month,
+     * sorted descending (last to first).
+     * For example, October dates would come before May dates,
+     * because October has an 'r' in it.
+     * thus: (2004-07-01, 2005-01-02, 2007-01-01, 2032-05-03)
+     * would sort to
+     * (2005-01-02, 2007-01-01, 2032-05-03, 2004-07-01)
+     *
+     * @param unsortedDates - an unsorted list of dates
+     * @return the collection of dates now sorted as per the spec
+     */
+    public Collection<LocalDate> sortDates(List<LocalDate> unsortedDates) {
+        List<LocalDate> withR = new ArrayList<>();
+        List<LocalDate> withoutR = new ArrayList<>();
+
+        for (LocalDate date : unsortedDates) {
+            if (date.getMonth().toString().toLowerCase().contains("r")) {
+                withR.add(date);
+            } else {
+                withoutR.add(date);
+            }
+        }
+
+        withR.sort(Comparator.comparing(LocalDate::getYear).thenComparing(LocalDate::getMonth).thenComparing(LocalDate::getDayOfMonth));
+        withoutR.sort(Comparator.comparing(LocalDate::getYear).thenComparing(LocalDate::getMonth).thenComparing(LocalDate::getDayOfMonth).reversed());
+
+        List<LocalDate> sortedDates = new ArrayList<>();
+        sortedDates.addAll(withR);
+        sortedDates.addAll(withoutR);
+
+        return sortedDates;
+    }
+}
